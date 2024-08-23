3ce547216b34cdbc7d037df2c45f914c0c3da07f
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-08-23 14:02:15 +0300

    Add all necessary files

--- AnnouncementNerdy.Domain/obj/Debug/net8.0/AnnouncementNerdy.Domain.AssemblyInfo.cs
+++ AnnouncementNerdy.Domain/obj/Debug/net8.0/AnnouncementNerdy.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnnouncementNerdy.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb9548a76adf0a613bbe1639e7718c8d788f0ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnnouncementNerdy.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnnouncementNerdy.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
