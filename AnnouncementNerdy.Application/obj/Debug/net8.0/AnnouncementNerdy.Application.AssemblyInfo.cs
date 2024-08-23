0d40145e4583de602fa1902d143f45b3966e62c2
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-08-23 10:18:54 +0300

    fix docker file, add env variables, add some logging

--- AnnouncementNerdy.Application/obj/Debug/net8.0/AnnouncementNerdy.Application.AssemblyInfo.cs
+++ AnnouncementNerdy.Application/obj/Debug/net8.0/AnnouncementNerdy.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnnouncementNerdy.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb9d90be5002317ed0bc83a6ba18d1a823ed6da")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnnouncementNerdy.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnnouncementNerdy.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
